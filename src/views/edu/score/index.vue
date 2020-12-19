d27e24430d5159be6721f01fd7c656ae5f2be9ba
尤佳楠 <405771331@qq.com>
2020-12-19 22:55:59 +0800

    feat: 完成成绩导入模块

--- src/views/edu/score/index.vue
+++ src/views/edu/score/index.vue
@@ -8,6 +8,17 @@
       :http-request="handleFileUpload">
       <el-button type="success" icon="el-icon-upload">成绩导入</el-button>
     </el-upload>
+
+    <el-divider>导入数据一览</el-divider>
+
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="candidateNo" label="准考证号"></el-table-column>
+      <el-table-column prop="realName" label="考生姓名"></el-table-column>
+      <el-table-column prop="examNo" label="考试编号"></el-table-column>
+      <el-table-column prop="examName" label="考试名称"></el-table-column>
+      <el-table-column prop="score" label="考试成绩"></el-table-column>
+      <el-table-column prop="pass" label="是否合格"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -15,10 +26,9 @@
 import * as api from '@/api/edu/score'
 
 export default {
-  // inject: ['reload'],
   data() {
     return {
-      
+      tableData: [],
     }
   },
 
@@ -46,6 +56,7 @@ export default {
       formData.append("file", file)
       api.uploadExcel(formData).then(res => {
         const { data } = res
+        this.tableData = data
         this.$message.success('导入成功')
       })
     },
